5f448b72335188c1d139eed0bcc86ffc43686a17
EveryoneStandUp <wjsgudwns98@naver.com>
2023-04-25 16:43:03 +0900

    first

---
+++ src/main/java/com/example/demo/Prj123230425Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj123230425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj123230425Application.class, args);
+	}
+
+}
